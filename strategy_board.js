9e8d6845ba5a9c3de785ae5d82ca127eb96d3c16
oZabu <83933546+oZabu@users.noreply.github.com>
2024-12-12 11:43:06 +0900

    Add files via upload

---
+++ strategy_board.js
@@ -0,0 +1,499 @@
+const piece1 = document.getElementById("piece1");
+const piece2 = document.getElementById("piece2");
+const piece3 = document.getElementById("piece3");
+const piece4 = document.getElementById("piece4");
+const piece5 = document.getElementById("piece5");
+const piece6 = document.getElementById("piece6");
+const piece7 = document.getElementById("piece7");
+const piece8 = document.getElementById("piece8");
+const piece9 = document.getElementById("piece9");
+const piece10 = document.getElementById("piece10");
+const piece11 = document.getElementById("piece11");
+const piece12 = document.getElementById("piece12");
+const piece13 = document.getElementById("piece13");
+const piece14 = document.getElementById("piece14");
+const piece15 = document.getElementById("piece15");
+const piece16 = document.getElementById("piece16");
+const piece17 = document.getElementById("piece17");
+const piece18 = document.getElementById("piece18");
+const piece19 = document.getElementById("piece19");
+const piece20 = document.getElementById("piece20");
+const piece21 = document.getElementById("piece21");
+const piece22 = document.getElementById("piece22");
+const piece23 = document.getElementById("piece23");
+const piece24 = document.getElementById("piece24");
+const piece25 = document.getElementById("piece25");
+const piece26 = document.getElementById("piece26");
+const piece27 = document.getElementById("piece27");
+const piece28 = document.getElementById("piece28");
+const piece29 = document.getElementById("piece29");
+const piece30 = document.getElementById("piece30");
+const piece31 = document.getElementById("piece31");
+const piece32 = document.getElementById("piece32");
+
+////ボタン画像
+var pieceBu1 = document.getElementById("pieceBu1");
+var pieceBu2 = document.getElementById("pieceBu2");
+var pieceBu3 = document.getElementById("pieceBu3");
+var pieceBu4 = document.getElementById("pieceBu4");
+var pieceBu5 = document.getElementById("pieceBu5");
+var pieceBu6 = document.getElementById("pieceBu6");
+var pieceBu7 = document.getElementById("pieceBu7");
+var pieceBu8 = document.getElementById("pieceBu8");
+var pieceBu9 = document.getElementById("pieceBu9");
+var pieceBu10 = document.getElementById("pieceBu10");
+var pieceBu11 = document.getElementById("pieceBu11");
+var pieceBu12 = document.getElementById("pieceBu12");
+var pieceBu13 = document.getElementById("pieceBu13");
+var pieceBu14 = document.getElementById("pieceBu14");
+var pieceBu15 = document.getElementById("pieceBu15");
+var pieceBu16 = document.getElementById("pieceBu16");
+var pieceBu17 = document.getElementById("pieceBu17");
+var pieceBu18 = document.getElementById("pieceBu18");
+var pieceBu19 = document.getElementById("pieceBu19");
+var pieceBu20 = document.getElementById("pieceBu20");
+var pieceBu21 = document.getElementById("pieceBu21");
+var pieceBu22 = document.getElementById("pieceBu22");
+var pieceBu23 = document.getElementById("pieceBu23");
+var pieceBu24 = document.getElementById("pieceBu24");
+var pieceBu25 = document.getElementById("pieceBu25");
+var pieceBu26 = document.getElementById("pieceBu26");
+var pieceBu27 = document.getElementById("pieceBu27");
+var pieceBu28 = document.getElementById("pieceBu28");
+var pieceBu29 = document.getElementById("pieceBu29");
+var pieceBu30 = document.getElementById("pieceBu30");
+var pieceBu31 = document.getElementById("pieceBu31");
+var pieceBu32 = document.getElementById("pieceBu32");
+
+function piecedisplay() {
+    if (piece1.checked) {
+        var off = document.getElementById('p1');
+        off.style.display = 'inline';
+        pieceBu1.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p1');
+        on.style.display = 'none';
+        pieceBu1.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece2.checked) {
+        var off = document.getElementById('p2');
+        off.style.display = 'inline';
+        pieceBu2.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p2');
+        on.style.display = 'none';
+        pieceBu2.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece3.checked) {
+        var off = document.getElementById('p3');
+        off.style.display = 'inline';
+        pieceBu3.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p3');
+        on.style.display = 'none';
+        pieceBu3.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece4.checked) {
+        var off = document.getElementById('p4');
+        off.style.display = 'inline';
+        pieceBu4.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p4');
+        on.style.display = 'none';
+        pieceBu4.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece5.checked) {
+        var off = document.getElementById('p5');
+        off.style.display = 'inline';
+        pieceBu5.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p5');
+        on.style.display = 'none';
+        pieceBu5.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece6.checked) {
+        var off = document.getElementById('p6');
+        off.style.display = 'inline';
+        pieceBu6.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p6');
+        on.style.display = 'none';
+        pieceBu6.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece7.checked) {
+        var off = document.getElementById('p7');
+        off.style.display = 'inline';
+        pieceBu7.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p7');
+        on.style.display = 'none';
+        pieceBu7.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece8.checked) {
+        var off = document.getElementById('p8');
+        off.style.display = 'inline';
+        pieceBu8.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p8');
+        on.style.display = 'none';
+        pieceBu8.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece9.checked) {
+        var off = document.getElementById('p9');
+        off.style.display = 'inline';
+        pieceBu9.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p9');
+        on.style.display = 'none';
+        pieceBu9.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece10.checked) {
+        var off = document.getElementById('p10');
+        off.style.display = 'inline';
+        pieceBu10.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p10');
+        on.style.display = 'none';
+        pieceBu10.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece11.checked) {
+        var off = document.getElementById('p11');
+        off.style.display = 'inline';
+        pieceBu11.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p11');
+        on.style.display = 'none';
+        pieceBu11.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece12.checked) {
+        var off = document.getElementById('p12');
+        off.style.display = 'inline';
+        pieceBu12.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p12');
+        on.style.display = 'none';
+        pieceBu12.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece13.checked) {
+        var off = document.getElementById('p13');
+        off.style.display = 'inline';
+        pieceBu13.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p13');
+        on.style.display = 'none';
+        pieceBu13.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece14.checked) {
+        var off = document.getElementById('p14');
+        off.style.display = 'inline';
+        pieceBu14.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p14');
+        on.style.display = 'none';
+        pieceBu14.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece15.checked) {
+        var off = document.getElementById('p15');
+        off.style.display = 'inline';
+        pieceBu15.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p15');
+        on.style.display = 'none';
+        pieceBu15.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece16.checked) {
+        var off = document.getElementById('p16');
+        off.style.display = 'inline';
+        pieceBu16.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p16');
+        on.style.display = 'none';
+        pieceBu16.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece17.checked) {
+        var off = document.getElementById('p17');
+        off.style.display = 'inline';
+        pieceBu17.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p17');
+        on.style.display = 'none';
+        pieceBu17.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece18.checked) {
+        var off = document.getElementById('p18');
+        off.style.display = 'inline';
+        pieceBu18.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p18');
+        on.style.display = 'none';
+        pieceBu18.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece19.checked) {
+        var off = document.getElementById('p19');
+        off.style.display = 'inline';
+        pieceBu19.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p19');
+        on.style.display = 'none';
+        pieceBu19.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece20.checked) {
+        var off = document.getElementById('p20');
+        off.style.display = 'inline';
+        pieceBu20.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p20');
+        on.style.display = 'none';
+        pieceBu20.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece21.checked) {
+        var off = document.getElementById('p21');
+        off.style.display = 'inline';
+        pieceBu21.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p21');
+        on.style.display = 'none';
+        pieceBu21.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece22.checked) {
+        var off = document.getElementById('p22');
+        off.style.display = 'inline';
+        pieceBu22.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p22');
+        on.style.display = 'none';
+        pieceBu22.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece23.checked) {
+        var off = document.getElementById('p23');
+        off.style.display = 'inline';
+        pieceBu23.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p23');
+        on.style.display = 'none';
+        pieceBu23.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece24.checked) {
+        var off = document.getElementById('p24');
+        off.style.display = 'inline';
+        pieceBu24.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p24');
+        on.style.display = 'none';
+        pieceBu24.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece25.checked) {
+        var off = document.getElementById('p25');
+        off.style.display = 'inline';
+        pieceBu25.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p25');
+        on.style.display = 'none';
+        pieceBu25.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece26.checked) {
+        var off = document.getElementById('p26');
+        off.style.display = 'inline';
+        pieceBu26.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p26');
+        on.style.display = 'none';
+        pieceBu26.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece27.checked) {
+        var off = document.getElementById('p27');
+        off.style.display = 'inline';
+        pieceBu27.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p27');
+        on.style.display = 'none';
+        pieceBu27.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece28.checked) {
+        var off = document.getElementById('p28');
+        off.style.display = 'inline';
+        pieceBu28.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p28');
+        on.style.display = 'none';
+        pieceBu28.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece29.checked) {
+        var off = document.getElementById('p29');
+        off.style.display = 'inline';
+        pieceBu29.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p29');
+        on.style.display = 'none';
+        pieceBu29.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece30.checked) {
+        var off = document.getElementById('p30');
+        off.style.display = 'inline';
+        pieceBu30.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p30');
+        on.style.display = 'none';
+        pieceBu30.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece31.checked) {
+        var off = document.getElementById('p31');
+        off.style.display = 'inline';
+        pieceBu31.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p31');
+        on.style.display = 'none';
+        pieceBu31.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+
+    if (piece32.checked) {
+        var off = document.getElementById('p32');
+        off.style.display = 'inline';
+        pieceBu32.style.backgroundColor = 'black';
+    } else {
+        var on = document.getElementById('p32');
+        on.style.display = 'none';
+        pieceBu32.style.backgroundColor = 'rgb(43, 44, 51)';
+    };
+};
+
+
+(function () {
+
+    //要素の取得
+    var elements = document.getElementsByClassName("drag-and-drop");
+
+    //要素内のクリックされた位置を取得するグローバル（のような）変数
+    var x;
+    var y;
+
+    //マウスが要素内で押されたとき、又はタッチされたとき発火
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].addEventListener("mousedown", mdown, false);
+        elements[i].addEventListener("touchstart", mdown, false);
+    }
+
+    //マウスが押された際の関数
+    function mdown(e) {
+
+        //クラス名に .drag を追加
+        this.classList.add("drag");
+
+        //タッチデイベントとマウスのイベントの差異を吸収
+        if (e.type === "mousedown") {
+            var event = e;
+        } else {
+            var event = e.changedTouches[0];
+        }
+
+        //要素内の相対座標を取得
+        x = event.pageX - this.offsetLeft;
+        y = event.pageY - this.offsetTop;
+
+        //ムーブイベントにコールバック
+        document.body.addEventListener("mousemove", mmove, false);
+        document.body.addEventListener("touchmove", mmove, false);
+    }
+
+    //マウスカーソルが動いたときに発火
+    function mmove(e) {
+
+        //ドラッグしている要素を取得
+        var drag = document.getElementsByClassName("drag")[0];
+
+        //同様にマウスとタッチの差異を吸収
+        if (e.type === "mousemove") {
+            var event = e;
+        } else {
+            var event = e.changedTouches[0];
+        }
+
+        //フリックしたときに画面を動かさないようにデフォルト動作を抑制
+        e.preventDefault();
+
+        //マウスが動いた場所に要素を動かす
+        drag.style.top = event.pageY - y + "px";
+        drag.style.left = event.pageX - x + "px";
+
+        //マウスボタンが離されたとき、またはカーソルが外れたとき発火
+        drag.addEventListener("mouseup", mup, false);
+        document.body.addEventListener("mouseleave", mup, false);
+        drag.addEventListener("touchend", mup, false);
+        document.body.addEventListener("touchleave", mup, false);
+
+    }
+
+    //マウスボタンが上がったら発火
+    function mup(e) {
+        var drag = document.getElementsByClassName("drag")[0];
+
+        //ムーブベントハンドラの消去
+        document.body.removeEventListener("mousemove", mmove, false);
+        drag.removeEventListener("mouseup", mup, false);
+        document.body.removeEventListener("touchmove", mmove, false);
+        drag.removeEventListener("touchend", mup, false);
+
+        //クラス名 .drag も消す
+        drag.classList.remove("drag");
+    }
+
+})()
+
+
+let txt = "";
+let slider = "";
+let img = "";
+
+// ロードが間に合わないので1秒遅延※環境によっては不要
+setTimeout(() => {
+    // HTMLの要素を取得
+    txt = document.getElementById("txt");
+    img = document.getElementById("preview");
+    slider = document.getElementById("slider");
+
+    //イベントリスナーの設定
+    slider.addEventListener("input", reSize);
+}, 1000);
+
+//「アクション」の設定
+const reSize = (e) => {
+    // テキストを更新
+    txt.innerHTML = e.target.value;
+    // 画像のサイズを変更
+    img.style.width = e.target.value + "px";
+};
+
+
+
+
+setInterval(piecedisplay, 100);
